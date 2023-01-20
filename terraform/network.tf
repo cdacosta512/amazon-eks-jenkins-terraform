1a6ad78ccc04ca0e77236157b6571a6c2f3d299b
cdacosta512 <codydacosta24@gmail.com>
2023-01-20 10:10:05 -0600

    adding changed terraform and jenkins files. Terraform has already been applied. Adding changes to be tracked.

--- terraform/network.tf
+++ terraform/network.tf
@@ -8,7 +8,7 @@ resource "aws_vpc" "development-vpc" {
 resource "aws_subnet" "public-subnet-1" {
   cidr_block        = "${var.public_subnet_1_cidr}"
   vpc_id            = "${aws_vpc.development-vpc.id}"
-  availability_zone = "${var.region}a"
+  availability_zone = "${var.region}b"
   tags = {
     Name = "${var.environment}-Public-Subnet-1"
   }
@@ -16,7 +16,7 @@ resource "aws_subnet" "public-subnet-1" {
 resource "aws_subnet" "public-subnet-2" {
   cidr_block        = "${var.public_subnet_2_cidr}"
   vpc_id            = "${aws_vpc.development-vpc.id}"
-  availability_zone = "${var.region}b"
+  availability_zone = "${var.region}c"
   tags = {
     Name = "${var.environment}-Public-Subnet-2"
   }
@@ -50,7 +50,7 @@ resource "aws_route_table_association" "public-route-3-association" {
 resource "aws_subnet" "private-subnet-1" {
   cidr_block        = "${var.private_subnet_1_cidr}"
   vpc_id            = "${aws_vpc.development-vpc.id}"
-  availability_zone = "${var.region}a"
+  availability_zone = "${var.region}b"
   tags = {
     Name = "${var.environment}-Private-Subnet-1"
   }
@@ -58,7 +58,7 @@ resource "aws_subnet" "private-subnet-1" {
 resource "aws_subnet" "private-subnet-2" {
   cidr_block        = "${var.private_subnet_2_cidr}"
   vpc_id            = "${aws_vpc.development-vpc.id}"
-  availability_zone = "${var.region}b"
+  availability_zone = "${var.region}c"
   tags = {
     Name = "${var.environment}-Private-Subnet-2"
   }
@@ -102,7 +102,7 @@ resource "aws_nat_gateway" "nat-gw" {
   tags = {
     Name = "${var.environment}-NATGW"
   }
-  depends_on = ["aws_eip.elastic-ip-for-nat-gw"]
+  depends_on = [aws_eip.elastic-ip-for-nat-gw]
 }
 resource "aws_route" "nat-gw-route" {
   route_table_id         = "${aws_route_table.private-route-table.id}"
